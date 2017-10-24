806cc1bac9b40c2d37d4d11da8ca517781156fa4
Nardberjean <Nardberjean@users.noreply.github.com>
2017-10-24 08:39:52 -0400

    Update install.bash

--- scripts/install.bash
+++ scripts/install.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ev # https://docs.travis-ci.com/user/customizing-the-build/
+# Returns to initial directory and can maybe be "sourced"
 pushd `mktemp --directory`
 
 pwd # do something here
